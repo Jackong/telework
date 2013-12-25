0999959f6869f705edae5d2c9d37439ed673ab99
zhemdaisy <zhemdaisy@gmail.com>
2013-12-25 22:31:51 +0800

    add text handler

---
+++ service/wechat/telework/text.class.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * User: daisy
+ * Date: 13-12-25
+ * Time: 下午8:42
+ */
+
+namespace service\wechat\telework;
+
+
+use service\wechat\Handler;
+use util\Log;
+
+class Text extends Handler {
+    public function handle(\SimpleXMLElement $subject) {
+        $toUserName = $subject->ToUserName;
+        $fromUserName = $subject->FromUserName;
+        $createTime = $subject->CreateTime;
+        $event = $subject->Event;
+        Log::Debug("$toUserName, $fromUserName, $createTime, $event");
+    }
+
+} 

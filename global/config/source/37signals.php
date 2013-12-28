380df88f083d4d8d5dc3fa6865a07e7891d2d5f9
zhemdaisy <zhemdaisy@gmail.com>
2013-12-28 11:49:42 +0800

    update 37signals config

--- global/config/source/37signals.php
+++ global/config/source/37signals.php
@@ -8,12 +8,26 @@
 class _37Signals {
     public static $host = "https://weworkremotely.com";
     public static $categories = array(
-        "1" => array("Design", "设计"),
-        "2" => array("Programming", "编程开发"),
-        "3" => array("Business/Exec", "商务师"),
-        "4" => array("Miscellaneous", "杂项"),
-        "5" => array("Copywriter", "广告文字撰稿人"),
-        "6" => array("System administration", "系统管理员"),
-        "7" => array("Customer Service/Support", "客服支持"),
+        "1" => array(
+            "lang" => array("Design", "设计"),
+        ),
+        "2" => array(
+            "lang" => array("Programming", "编程开发"),
+        ),
+        "3" => array(
+            "lang" => array("Business/Exec", "商务师"),
+        ),
+        "4" => array(
+            "lang" => array("Miscellaneous", "杂项"),
+        ),
+        "5" => array(
+            "lang" => array("Copywriter", "广告文字撰稿人"),
+        ),
+        "6" => array(
+            "lang" => array("System administration", "系统管理员"),
+        ),
+        "7" => array(
+            "lang" => array("Customer Service/Support", "客服支持"),
+        ),
     );
 }

4538447d0c974676f1fd3711027126f941fedab4
zhemdaisy <zhemdaisy@gmail.com>
2014-01-08 09:13:31 +0800

    add index for light app

--- tpl/light/app.php
+++ tpl/light/app.php
@@ -1,10 +1,60 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title></title>
+    <title>自由人</title>
     <meta name="baidu-tc-cerfication" content="6bd457adcf08474cc1250e10dfb0ab6d" />
+    <?php include __DIR__ . "/../header.php"; ?>
+
+    <!-- Custom styles for this template -->
+    <link href="/css/justified-nav.css" rel="stylesheet">
 </head>
 <body>
-<?= $this->text; ?>
+    <div class="container">
+
+        <div class="masthead">
+            <h3 class="text-muted">自由人</h3>
+            <ul class="nav nav-justified">
+                <li class="active"><a href="#">招聘资讯</a></li>
+                <li><a href="#">求职</a></li>
+                <li><a href="#">招聘</a></li>
+            </ul>
+        </div>
+
+        <!-- Jumbotron -->
+        <div class="jumbotron">
+            <h1>远程工作</h1>
+            <p class="lead">自由人为您提供远程工作求职、招聘服务。</p>
+            <p>
+                <a class="btn btn-lg btn-success" href="#" role="button">登录</a>
+                <a class="btn btn-lg btn-success" href="#" role="button">注册</a>
+            </p>
+        </div>
+
+        <!-- Example row of columns -->
+        <div class="row">
+            <div class="col-lg-4">
+                <h2>Safari bug warning!</h2>
+                <p class="text-danger">Safari exhibits a bug in which resizing your browser horizontally causes rendering errors in the justified nav that are cleared upon refreshing.</p>
+                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Heading</h2>
+                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Heading</h2>
+                <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
+                <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
+            </div>
+        </div>
+
+        <!-- Site footer -->
+        <div class="footer">
+            <p>&copy; Company 2013</p>
+        </div>
+
+    </div> <!-- /container -->
 </body>
 </html>
--- tpl/test.php
+++ tpl/test.php
@@ -2,16 +2,7 @@
 <html>
 <head>
     <title><?= $this->title; ?></title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="http://cdn.bootcss.com/twitter-bootstrap/3.0.3/css/bootstrap.min.css">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="http://cdn.bootcss.com/html5shiv/3.7.0/html5shiv.min.js"></script>
-    <script src="http://cdn.bootcss.com/respond.js/1.3.0/respond.min.js"></script>
-    <![endif]-->
+    <?php include __DIR__ . "/header.php";?>
 </head>
 <body>
 <h1><?= $this->body; ?></h1>

b4a1346befea43af2f6e4b9a9d1a4cd8f6d62a07
zhemdaisy <zhemdaisy@gmail.com>
2014-01-09 10:33:22 +0800

    add utf-8 meta on header.tpl

--- tpl/header.php
+++ tpl/header.php
@@ -1,3 +1,4 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <!-- Bootstrap -->
 <link rel="stylesheet" href="http://cdn.bootcss.com/twitter-bootstrap/3.0.3/css/bootstrap.min.css">

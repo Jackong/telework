9bac923ada599857c8f567d32d3a6444a5393895
zhemdaisy <zhemdaisy@gmail.com>
2014-01-13 21:37:23 +0800

    add link for sub and recruit

---
+++ router/light/recruit.class.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * User: daisy
+ * Date: 14-1-5
+ * Time: 下午8:37
+ */
+
+namespace router\light;
+
+
+use router\Router;
+
+class Recruit extends Router {
+    public function get() {
+        return "developing";
+    }
+
+} 
--- tpl/light/app.php
+++ tpl/light/app.php
@@ -8,34 +8,21 @@
 
   <body>
 
-    <nav class="navbar navbar-fixed-top navbar-inverse" role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="index.php">自由人</a>
-        </div>
-
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse navbar-ex1-collapse">
-          <ul class="nav navbar-nav">
-            <li><a href="#about">求职</a></li>
-            <li><a href="#services">招聘</a></li>
-          </ul>
-        </div><!-- /.navbar-collapse -->
-      </div><!-- /.container -->
-    </nav>
-    
     <div class="container">
 
       <div class="jumbotron hero-spacer">
         <h1>自由人</h1>
         <p>国内首个专注于提供远程办公求职招聘服务的平台，愿求职者找到满意工作，愿招聘者找到合意人才。</p>
-        <p><a class="btn btn-primary btn-large">订阅职位</a> <a class="btn btn-info btn-large">发布招聘</a></p>
+        <form class="form-inline" role="form" action="/light/subscription">
+            <div class="form-group">
+                <label class="sr-only" for="email">Email</label>
+                <input type="email" class="form-control" id="email" placeholder="Email">
+            </div>
+
+            <button type="submit" class="btn btn-primary">订阅职位</button>
+
+            <a class="btn btn-link" href="/light/recruit">发布招聘</a>
+        </form>
       </div>
       
       <hr>

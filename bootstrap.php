affda997c9c023ac44aab3627eaa3be3af104816
JackongChange <JackongC@gmail.com>
2014-01-19 09:26:46 +0800

    fix: remove lower for className on loader

--- bootstrap.php
+++ bootstrap.php
@@ -1,11 +1,11 @@
 <?php
 define("PROJECT", __DIR__);
-require_once "glob/time.php";
+require_once PROJECT . "/glob/time.php";
 
 function loader($className) {
     list($namespace) = explode("\\", $className);
     if (in_array($namespace, array("service", "glob", "util", "cron", "common"))) {
-        $file = strtolower(str_replace("\\", "/", $className));
+        $file = str_replace("\\", "/", $className);
         require_once PROJECT . "/$file.php";
     }
 }

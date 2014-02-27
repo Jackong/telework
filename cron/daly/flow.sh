2b4c243229b30d8a557d150a8dd9011f447ec568
JackongChange <JackongC@gmail.com>
2014-02-27 08:38:36 +0800

    fix daly flow shell

--- cron/daly/flow.sh
+++ cron/daly/flow.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-date=$(date -d '0 days' +"%Y-%m-%d")
+date=$(date -d '-1 days' +"%Y-%m-%d")
 result="/home/bae/log/flow.log.$date"
-echo $date >>$result
 grep "router\\\light\\\Jobs:jobs" /home/bae/log/user.log.$date | awk -F '|' '{if(length($2)==32){print $3"|"$2}}' | sort | uniq >/tmp/flow.log
-awk -F '|' '{
+awk -v date=$date -F '|' '{
         categories[$1]+=1;
     }END{
             for(category in categories){
-                print category":"categories[category]
+                print date","category","categories[category]
             }
     }' /tmp/flow.log >>$result
 
-echo "The number of the users:" >>$result
-awk -F '|' '{print $2}' /tmp/flow.log | sort | uniq | wc -l >>$result
+users=$(awk -F '|' '{print $2}' /tmp/flow.log | sort | uniq | wc -l)
+echo "$date,total,$users" >>$result
+rm /tmp/flow.log
